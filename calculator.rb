cbfd73659732d92fc743c738a48daee609146cd0
Pedro Vega <pedro.vega@orbis.com.pe>
2016-08-16 10:12:34 -0500

    WIP 2

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,17 @@
 # encoding: utf-8
 
+class Integer
+    def factorial
+        f = 1 
+        
+        for i in 1..self
+            f *= i 
+        end 
+        
+        f
+    end
+end
+
 def get_int_values(n)
 	n.times.map.with_index { |n|
 		print "Ingresa #{1 + n}° valor: "
@@ -7,28 +19,51 @@ def get_int_values(n)
 	}
 end
 
-puts '¿Qué quieres hacer: [sumar], [restar] o [multiplicar]'
-
-operation = gets.chomp
-
-puts '¿Cuántos valores?'
-
-num_of_values = gets.to_i
-
-case operation.downcase
-when 'sumar'
-	puts '¿Qué números quiere sumar?'
-	operator = :+
-when 'restar'
-	puts '¿Qué números quiere restar?'
-	operator = :-
-when 'multiplicar'
-	puts '¿Qué números quiere multiplicar'
-	operator = :*
+def factorial(n)
+    n.factorial
 end
 
-answer = get_int_values(num_of_values).inject(operator)
+while true do
+    puts '¿Qué quieres hacer: [sumar], [restar], [multiplicar], [factorial] o [salir]'
 
-puts "La respuesta es ... #{answer}"
+    operation = gets.chomp
+    
+    operation.downcase!
+
+    case operation
+    when 'sumar'
+	    operator = :+
+    when 'restar'
+	    operator = :-
+    when 'multiplicar'
+	    operator = :*
+    when 'factorial'
+        operator = :!
+    when 'salir'
+        break
+    else
+        puts 'Ingrese una operación correcta'
+    end
+
+    if %w(sumar restar multiplicar).include? operation
+        puts '¿Cuántos valores?'
+
+        num_of_values = gets.to_i    
+
+	    puts "¿Qué números quiere #{operation}?"
+
+        answer = get_int_values(num_of_values).inject(operator)
+
+        puts "La respuesta es ... #{answer}"
+    else
+        puts "Ingrese el número"
+        
+        n = gets.chomp.to_i
+        
+        answer = factorial(n)
+        
+        puts "La respuesta es ... #{answer}"
+    end
+end
 
 	
